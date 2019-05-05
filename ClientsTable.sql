e23acdcbc83d24ce5433fcf3c22de7ac8d3677af
Victor Almeida <vala@cesar.com>
2019-05-05 17:30:29 -0300

    Our First database

---
+++ ClientsTable.sql
@@ -0,0 +1,8 @@
+use Cadastro;
+create table Clientes(
+    Nome varchar(30),
+    Bairro varchar(50),
+    Sexo char(1),
+    Idade tinyint(3)
+);
+
---
+++ OrderTable.sql
@@ -0,0 +1,7 @@
+use Cadastro;
+
+create table Pedidos(
+    Local_de_entrega varchar(50),
+    Preco varchar(10),
+    Presente char(1)
+);
